376f7a531c8ea960670abefc9e625596ea64f751
anhquoc0912 <leanhquoc1611@gmail.com>
2024-10-12 19:30:28 +0700

    add Bottom MainScreen

---
+++ lib/widgets/bottomNav/BottomNav.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class BottomNavItem extends StatelessWidget {
+  final IconData icon;
+ //final String label;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  const BottomNavItem({
+    Key? key,
+    required this.icon,
+    //required this.label,
+    required this.isSelected,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        color: Colors.transparent,
+        padding: const EdgeInsets.symmetric(
+            vertical: 12.0,
+            horizontal: 16.0), // Thay đổi khoảng cách để mở rộng vùng bấm
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              icon,
+              color: isSelected ? Colors.blue : Colors.grey,
+              size: 35,
+            ),
+            // Text(
+            //   label,
+            //   style: TextStyle(
+            //     color: isSelected ? Colors.blue : Colors.grey,
+            //   ),
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+}
