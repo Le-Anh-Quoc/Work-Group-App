ccb02365ef25b28c7d296a30d35ac9265882740e
anhquoc0912 <leanhquoc1611@gmail.com>
2024-10-12 19:22:17 +0700

    update UI login, signup

---
+++ lib/screens/authentication/LoginScreen.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+import 'package:ruprup/screens/authentication/SignUpScreen.dart';
+import 'package:ruprup/screens/MainScreen.dart';
+import 'package:ruprup/services/auth_service.dart';
+import 'package:ruprup/widgets/auth/ButtonWidget.dart';
+import 'package:ruprup/widgets/auth/FieldWidget.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _auth = AuthService();
+
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+
+  bool isLoading = false;
+
+  void _handleLogin() {
+    print('nhấn nút đăng nhập');
+    if (_email.text.isNotEmpty && _password.text.isNotEmpty) {
+      setState(() {
+        isLoading = true;
+      });
+      _auth.logIn(_email.text, _password.text).then((user) {
+        if (user != null) {
+          setState(() {
+            isLoading = false;
+          });
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) => MainScreen(selectedIndex: 0)),
+          );
+        } else {
+          setState(() {
+            isLoading = false;
+          });
+        }
+      });
+    } else {
+      print("Please fill form correctly");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: isLoading
+          ? Center(
+              child: SizedBox(
+                height: size.height / 20,
+                width: size.width / 20,
+                child: CircularProgressIndicator(),
+              ),
+            )
+          : SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(40.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(height: 50),
+                    Center(
+                      child: Container(
+                        width: 90, // Chiều rộng của container
+                        height: 90, // Chiều cao của container
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30), // Bo tròn
+                          image: const DecorationImage(
+                            image: NetworkImage(
+                                'https://www.workingskills.net/wp-content/plugins/profilegrid-user-profiles-groups-and-communities/public/partials/images/default-group.png'), // Hình ảnh logo ngẫu nhiên
+                            fit: BoxFit
+                                .cover, // Chỉnh kích thước hình ảnh phù hợp với container
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Text(
+                      "Rup Rup",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Text(
+                      "Promote the spirit of Teamwork",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 25),
+                    const Text(
+                      "Login",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      "Enter your email and password",
+                      style: TextStyle(
+                        fontSize: 17,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    FieldWidget(
+                      icon: const Icon(Icons.email_outlined),
+                      nameField: 'Email',
+                      isPass: false,
+                      controller: _email,
+                    ),
+                    const SizedBox(height: 20),
+                    FieldWidget(
+                        icon: const Icon(Icons.lock_outline),
+                        nameField: 'Password',
+                        isPass: true,
+                        controller: _password),
+                    //const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            // Add functionality for forgot password
+                          },
+                          child: const Text(
+                            "Forgot password?",
+                            style: TextStyle(
+                                color: Colors.red, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    ButtonWidget(nameButton: "Log in", onTap: _handleLogin),
+                    const SizedBox(height: 30),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Don't have an account? "),
+                        GestureDetector(
+                          onTap: () => Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (_) => const SignUpScreen(),
+                            ),
+                          ),
+                          child: const Text(
+                            "Sign Up",
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 40),
+                  ],
+                ),
+              ),
+            ),
+    );
+  }
+
+  // Widget customButton(Size size) {
+  //   return GestureDetector(
+  //     onTap: () {
+  //       if (_email.text.isNotEmpty && _password.text.isNotEmpty) {
+  //         setState(() {
+  //           isLoading = true;
+  //         });
+  //         _auth.logIn(_email.text, _password.text).then((user) {
+  //           if (user != null) {
+  //             setState(() {
+  //               isLoading = false;
+  //             });
+  //             Navigator.push(
+  //               context,
+  //               MaterialPageRoute(builder: (_) => const HomeScreen()),
+  //             );
+  //           } else {
+  //             setState(() {
+  //               isLoading = false;
+  //             });
+  //           }
+  //         });
+  //       } else {
+  //         print("Please fill form correctly");
+  //       }
+  //     },
+  //     child: Container(
+  //       height: size.height / 14,
+  //       width: size.width / 1.2,
+  //       decoration: BoxDecoration(
+  //         borderRadius: BorderRadius.circular(10),
+  //         color: Colors.blue,
+  //       ),
+  //       alignment: Alignment.center,
+  //       child: const Text(
+  //         "Login",
+  //         style: TextStyle(color: Colors.white, fontSize: 22),
+  //       ),
+  //     ),
+  //   );
+  // }
+}
---
+++ lib/screens/authentication/SignUpScreen.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:ruprup/screens/authentication/LoginScreen.dart';
+import 'package:ruprup/screens/authentication/VerificationScreen.dart';
+import 'package:ruprup/services/auth_service.dart';
+import 'package:ruprup/utils/validators.dart'; // Import validators.dart
+import 'package:ruprup/widgets/auth/ButtonWidget.dart';
+import 'package:ruprup/widgets/auth/FieldWidget.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final AuthService _authService = AuthService();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Future<void> _handleRegister() async {
+    if (_formKey.currentState!.validate()) {
+      try {
+        await _authService.registerWithEmail(
+          _nameController.text,
+          _emailController.text,
+          _passwordController.text,
+        );
+        // Chuyển đến màn hình thông báo email đã được gửi
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (_) => const VerificationScreen()),
+        );
+      } catch (e) {
+        print(e);
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(e.toString())),
+        );
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Please fill in all fields correctly')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const SizedBox(height: 50),
+                Center(
+                  child: Container(
+                    width: 90, // Chiều rộng của container
+                    height: 90, // Chiều cao của container
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30), // Bo tròn
+                      image: const DecorationImage(
+                        image: NetworkImage(
+                            'https://www.workingskills.net/wp-content/plugins/profilegrid-user-profiles-groups-and-communities/public/partials/images/default-group.png'), // Hình ảnh logo ngẫu nhiên
+                        fit: BoxFit.cover, // Chỉnh kích thước hình ảnh phù hợp với container
+                      ),
+                    ),
+                  ),
+                ),
+                const Text(
+                  "Rup Rup",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Text(
+                  "Promote the spirit of Teamwork",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 25),
+                const Text(
+                  "Sign up",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  "Enter your credentials to continue",
+                  style: TextStyle(
+                    fontSize: 17,
+                    color: Colors.grey,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                FieldWidget(
+                  icon: const Icon(Icons.person_2_outlined),
+                  nameField: 'Name',
+                  isPass: false,
+                  controller: _nameController,
+                  validator: Validators.validateName,
+                ),
+                const SizedBox(height: 20),
+                FieldWidget(
+                  icon: const Icon(Icons.email_outlined),
+                  nameField: 'Email',
+                  isPass: false,
+                  controller: _emailController,
+                  validator: Validators.validateEmail,
+                ),
+                const SizedBox(height: 20),
+                FieldWidget(
+                    icon: const Icon(Icons.lock_outline),
+                    nameField: 'Password',
+                    isPass: true,
+                    controller: _passwordController,
+                    validator: Validators.validatePassword),
+                const SizedBox(height: 30),
+                ButtonWidget(nameButton: "Sign up", onTap: _handleRegister),
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Already have an account? "),
+                    GestureDetector(
+                      onTap: () => Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (_) => const LoginScreen(),
+                        ),
+                      ),
+                      child: const Text(
+                        "Log in",
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 40),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
