c764635aa4b10e23d9775d5b640290fec05bee54
anhquoc0912 <leanhquoc1611@gmail.com>
2024-10-12 19:31:18 +0700

    add TabBar in HomeScreen

---
+++ lib/widgets/chat/ToggleTextRow.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class ToggleTextRow extends StatefulWidget {
+  @override
+  _ToggleTextRowState createState() => _ToggleTextRowState();
+}
+
+class _ToggleTextRowState extends State<ToggleTextRow> {
+  // Biến theo dõi đoạn văn bản đang được chọn
+  String selectedText = 'Message'; // Mặc định "Message" được chọn
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                selectedText = 'Message';
+              });
+            },
+            child: Text(
+              "Message",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+                color: selectedText == 'Message' ? Colors.blue : Colors.grey,
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                selectedText = 'Group';
+              });
+            },
+            child: Text(
+              "Group",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+                color: selectedText == 'Group' ? Colors.blue : Colors.grey,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
