22c702a304f3564351b9d02503480d97514ee1d7
anhquoc0912 <leanhquoc1611@gmail.com>
2024-10-12 19:36:47 +0700

    update UI group widget in ListGroupScreen

---
+++ lib/widgets/group/GroupWidget.dart
@@ -0,0 +1,126 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:ruprup/models/group_model.dart';
+import 'package:ruprup/screens/group/GroupScreen.dart';
+import 'package:ruprup/services/group_service.dart'; // Import dịch vụ nhóm
+
+class GroupWidget extends StatelessWidget {
+  final String groupId;
+
+  const GroupWidget({
+    Key? key,
+    required this.groupId,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Group?>(
+      future: GroupService().getGroup(groupId), // Gọi hàm getGroup đã định nghĩa
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (snapshot.hasError) {
+          return const Center(child: Text('Error loading group'));
+        }
+
+        if (!snapshot.hasData) {
+          return const Center(child: Text('Group not found'));
+        }
+
+        // Lấy thông tin nhóm từ dữ liệu trả về
+        final group = snapshot.data!;
+
+        // Danh sách mẫu các hình ảnh
+        final List<String> allMemberImages = [
+          'https://randomuser.me/api/portraits/men/1.jpg',
+          'https://randomuser.me/api/portraits/men/2.jpg',
+          'https://randomuser.me/api/portraits/men/3.jpg',
+          'https://randomuser.me/api/portraits/women/1.jpg',
+          'https://randomuser.me/api/portraits/women/2.jpg',
+          'https://randomuser.me/api/portraits/women/3.jpg',
+        ];
+
+        final random = Random();
+        final shuffledImages = List<String>.from(allMemberImages)..shuffle(random);
+        final displayedImages = shuffledImages.take(4).toList(); // Lấy tối đa 4 ảnh ngẫu nhiên
+
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => GroupScreen(groupId: group.groupId)),
+            );
+          },
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            constraints: BoxConstraints(
+              minHeight: 150, // Đặt chiều cao tối thiểu
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white, // Đặt màu nền là trắng
+              borderRadius: BorderRadius.circular(12.0), // Bo tròn các góc
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5), // Màu sắc bóng
+                  spreadRadius: 1, // Độ lan tỏa của bóng
+                  blurRadius: 5, // Độ mờ của bóng
+                  offset: Offset(0, 3), // Vị trí của bóng (x,y)
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                        backgroundImage: NetworkImage(displayedImages[0]),
+                        radius: 22),
+                    const SizedBox(width: 8), // Khoảng cách giữa hai hình
+                    CircleAvatar(
+                      backgroundImage:
+                          NetworkImage(displayedImages[1]), // Hình thứ hai
+                      radius: 22,
+                    ),
+                    const SizedBox(width: 8),
+                    Container(
+                      width: 45,
+                      height: 45,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.blue, width: 2)),
+                      child: Center(
+                        child: Text(
+                          '+${displayedImages.length - 3}',
+                          style: const TextStyle(
+                            color: Colors.blue,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 16), // Khoảng cách giữa hình đại diện và tiêu đề
+                Text(
+                  group.groupName, // Sử dụng groupName từ dữ liệu nhóm
+                  style: const TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
