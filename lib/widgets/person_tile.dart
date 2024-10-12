071588eba48a760d1e9991d614451559afa6892a
anhquoc0912 <leanhquoc1611@gmail.com>
2024-10-12 19:35:24 +0700

    update UI widget ChooseMember in CreateGroupScreen

--- lib/widgets/person_tile.dart
+++ lib/widgets/person_tile.dart
@@ -49,35 +49,52 @@ class PersonTile extends StatelessWidget {
   }
 }
 
-class ChooseMemberGroupTile extends StatefulWidget {
+class ChooseMemberGroupTile extends StatelessWidget {
   final String name;
+  final bool isSelected;
+  final VoidCallback onSelected;
 
-  const ChooseMemberGroupTile({super.key, required this.name});
-
-  @override
-  _ChooseMemberGroupTileState createState() => _ChooseMemberGroupTileState();
-}
-
-class _ChooseMemberGroupTileState extends State<ChooseMemberGroupTile> {
-  bool isActive = false;
+  const ChooseMemberGroupTile({
+    super.key,
+    required this.name,
+    required this.isSelected,
+    required this.onSelected,
+  });
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      contentPadding: const EdgeInsets.all(8),
       leading: const CircleAvatar(
+        radius: 22,
         backgroundImage:
             NetworkImage('https://picsum.photos/200/300?random=${1}'),
       ),
-      title: Text(widget.name),
-      trailing: GestureDetector(
-        child: isActive ? const Icon(Icons.check_box) : const Icon(Icons.check_box_outline_blank),
+      title: Text(
+        name,
+        style: TextStyle(
+          fontWeight: isSelected
+              ? FontWeight.bold
+              : FontWeight.normal, // In đậm khi được chọn
+        ),
       ),
-      onTap: () {
-        setState(() {
-            isActive = !isActive; // Thay đổi trạng thái của isActive
-          });
-      },
+      trailing: GestureDetector(
+        child: isSelected
+            ? const Icon(
+                Icons.check_circle,
+                color: Colors.blue,
+                size: 28,
+              )
+            : const Icon(
+                Icons.circle_outlined,
+                color: Colors.blue,
+                size: 28,
+              ),
+      ),
+      tileColor: isSelected
+          ? Colors.grey.withOpacity(0.1)
+          : Colors.transparent, // Nền hơi ngả màu khi được chọn
+      onTap: onSelected, // Kích hoạt khi nhấn vào toàn bộ ListTile
     );
   }
 }
-
