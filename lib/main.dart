95f9bd9b8767e192a9dc2482d356186b93586a3a
anhquoc0912 <leanhquoc1611@gmail.com>
2024-10-12 19:45:41 +0700

    fix error when start APP

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,29 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:ruprup/screens/ChatScreen.dart';
-import 'package:ruprup/screens/HomeScreen.dart';
-
-import 'screens/LoginScreen.dart';
+import 'package:ruprup/screens/MainScreen.dart';
+import 'package:ruprup/screens/authentication/LoginScreen.dart';
+import 'package:ruprup/widgets/animation/hero.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
-  runApp(MyApp());
+  await Firebase.initializeApp();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      navigatorObservers: [CustomHeroController()],
+      home: const LoginScreen(),
+      //initialRoute: AppRoutes.home,
+      //routes: AppRoutes.routes, // Sử dụng routes tĩnh
+      // Hoặc bạn có thể sử dụng generateRoute
+      //onGenerateRoute: AppRoutes.generateRoute, // Sử dụng phương thức generateRoute
+      // Các thuộc tính khác
     );
   }
 }
