e4b75d43d2110e74f7e3f700cee6808a12f33a24
anhquoc0912 <leanhquoc1611@gmail.com>
2024-10-13 12:31:27 +0700

    check out the NotificationScreen

--- lib/screens/notification/NotificationScreen.dart
+++ lib/screens/notification/NotificationScreen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ruprup/services/notification_service.dart';
-import 'package:ruprup/widgets/notification/notification.dart';
+import 'package:ruprup/widgets/notification/NotificationWidget.dart';
 
 class NotificationScreen extends StatefulWidget {
   const NotificationScreen({super.key});
@@ -31,33 +31,44 @@ class _NotificationScreenState extends State<NotificationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios, color: Colors.black, size: 28), // Custom icon here
-          onPressed: () {
-            Navigator.of(context).pop();  // Handles back navigation
-          },
-        ),
         backgroundColor: Colors.white,
-        title: const Text("Notifications", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-        centerTitle: true,
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.delete_outline_outlined, size: 28))
-        ],
-      ),
-      body: _notificationResults.isEmpty
-          ? Center(child: Text('No notifications'))
-          : ListView.builder(
-              itemCount: _notificationResults.length,
-              itemBuilder: (context, index) {
-                return NotificationFriendRequest(
-                  message: _notificationResults[index]['message'],
-                  idSenderRequest: _notificationResults[index]['senderId'],
-                );
-              },
-            ),
-    );
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_ios,
+                color: Colors.black, size: 28), // Custom icon here
+            onPressed: () {
+              Navigator.of(context).pop(); // Handles back navigation
+            },
+          ),
+          backgroundColor: Colors.white,
+          title: const Text("Notifications",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+          centerTitle: true,
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.delete_outline_outlined, size: 28))
+          ],
+        ),
+        // body: _notificationResults.isEmpty
+        //     ? const Center(child: Text('No notifications'))
+        //     : ListView.builder(
+        //         itemCount: _notificationResults.length,
+        //         itemBuilder: (context, index) {
+        //           return const NotificationWidget(isInvite: true);
+        //         },
+        //       ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              NotificationWidget(notificationTypeString: 'friend'),
+              NotificationWidget(notificationTypeString: 'friendInvite'),
+              NotificationWidget(notificationTypeString: 'group'),
+              NotificationWidget(notificationTypeString: 'groupMeeting'),
+              NotificationWidget(notificationTypeString: 'task'),
+              NotificationWidget(notificationTypeString: 'project')
+            ],
+          ),
+        ));
   }
 }
