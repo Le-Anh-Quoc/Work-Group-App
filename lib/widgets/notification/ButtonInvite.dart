8bba76b6b04fa35c00241130ff2cb3d7a1c94c37
anhquoc0912 <leanhquoc1611@gmail.com>
2024-10-13 12:29:09 +0700

    add Widget Notification

---
+++ lib/widgets/notification/ButtonInvite.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+// button chấp nhận hoặc từ chối lời mời kết bạn (false: từ chối, true: chấp nhận)
+class ButtonInvite extends StatelessWidget {
+  final bool typeButton;
+  const ButtonInvite({super.key, required this.typeButton});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: typeButton ? Colors.blue : Colors.white,
+        foregroundColor: typeButton ? Colors.white : Colors.blue,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10)
+        )
+      ),
+      child: Text(typeButton ? 'Accept' : 'Refuse')
+    );
+  }
+}
---
+++ lib/widgets/notification/NotificationWidget.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import 'package:ruprup/models/notification_model.dart';
+import 'package:ruprup/widgets/notification/ButtonInvite.dart';
+
+class NotificationWidget extends StatelessWidget {
+  final String notificationTypeString; // Biến này là để nhận giá trị từ Firebase
+  late final NotificationType notificationType;
+  NotificationWidget({super.key, required this.notificationTypeString}) {
+    notificationType = NotificationTypeExtension.fromString(notificationTypeString);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // Thực hiện hành động khi chạm vào thông báo
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.shade300,
+                blurRadius: 10,
+                offset: const Offset(0, 5),
+              ),
+            ],
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildIcon(notificationType),
+              const SizedBox(width: 12),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Nội dung thông báo
+                    const Text(
+                      'John Doe đã gửi cho bạn một lời mời kết bạn!',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    // ignore: unrelated_type_equality_checks
+                    if (notificationType == NotificationType.friendInvite)
+                      const Row(
+                        children: [
+                          ButtonInvite(typeButton: true), // Nút Chấp nhận
+                          SizedBox(width: 10),
+                          ButtonInvite(typeButton: false), // Nút Từ chối
+                        ],
+                      ),
+                    const SizedBox(height: 8),
+                    // Thời gian thông báo
+                    const Text(
+                      '16 minutes ago',
+                      style: TextStyle(fontSize: 12, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget _buildIcon(NotificationType type) {
+  switch (type) {
+    case NotificationType.friend || NotificationType.friendInvite:
+      return const SizedBox(
+        width: 50,
+        height: 50,
+        child: CircleAvatar(
+          backgroundImage:
+              NetworkImage('https://picsum.photos/200/300?random=2'),
+          //radius: 25,
+        ),
+      );
+    case NotificationType.group:
+      return SizedBox(
+        width: 50,
+        height: 50,
+        child: Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.lightBlue.shade100, // Nền xanh nhạt
+          ),
+          padding: const EdgeInsets.all(8),
+          child: const Icon(
+            Icons.group_outlined, // Icon cho nhóm
+            color: Colors.blue,
+            size: 25,
+          ),
+        ),
+      );
+      case NotificationType.groupMeeting:
+      return SizedBox(
+        width: 50,
+        height: 50,
+        child: Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.lightBlue.shade100, // Nền xanh nhạt
+          ),
+          padding: const EdgeInsets.all(8),
+          child: const Icon(
+            Icons.videocam_outlined, // Icon cho cuộc họp meeting
+            color: Colors.blue,
+            size: 25,
+          ),
+        ),
+      );
+    case NotificationType.task:
+      return SizedBox(
+        width: 50,
+        height: 50,
+        child: Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.lightGreen.shade100, // Nền xanh nhạt
+          ),
+          padding: const EdgeInsets.all(8),
+          child: const Icon(
+            Icons.task_outlined, // Icon cho nhóm
+            color: Colors.green,
+            size: 25,
+          ),
+        ),
+      );
+    case NotificationType.project:
+      return SizedBox(
+        width: 50,
+        height: 50,
+        child: Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.orange.shade100, // Nền xanh nhạt
+          ),
+          padding: const EdgeInsets.all(8),
+          child: const Icon(
+            Icons.check_circle_outline, // Icon cho project
+            color: Colors.orange,
+            size: 25,
+          ),
+        ),
+      );
+    default:
+      return Container(); // Trường hợp không xác định
+  }
+}
