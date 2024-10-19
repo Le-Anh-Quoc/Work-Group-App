e2f4db6035782abb6eaaf75d8ab4bbf99143f372
anhquoc0912 <leanhquoc1611@gmail.com>
2024-10-19 07:29:31 +0700

    delete path in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:ruprup/models/group_model.dart';
 import 'package:ruprup/screens/MainScreen.dart';
 import 'package:ruprup/screens/authentication/LoginScreen.dart';
 import 'package:ruprup/screens/group/GroupScreen.dart';
